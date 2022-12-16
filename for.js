0f44184dcec88876fde824060ea14201983a5fe7
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2022-12-16 23:33:55 +0530

    ok

---
+++ for.js
@@ -0,0 +1,20 @@
+/*for(i=1;i<=100;i++){
+    console.log(i);
+}*/
+
+/*for(i=10;i>=1;i--){
+    console.log(i);
+}*/
+/*let i=10;
+while(i>=1){
+    console.log(i);
+    i--
+
+}*/
+let i=1
+do{
+    console.log(i);
+    i++
+
+}
+while(i<=10)
